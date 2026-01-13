562ddbe6babc88331b2eb411ee79e8d7a42001b5
camiloa.vega <camiloa.vega@fiscalia.gov.co>
2026-01-13 16:54:15 -0500

    Add AuthProvider at main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/lib/query-client";
+import { AuthProvider } from "@/contexts/auth-context";
 
 import "./index.css";
 import App from "./App.tsx";
@@ -9,7 +10,9 @@ import App from "./App.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
 );
