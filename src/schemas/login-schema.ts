2f6e5abdd82ef67cb579d8d232d477b78e327934
camiloa.vega <camiloa.vega@fiscalia.gov.co>
2026-01-16 11:39:07 -0500

    Fix typo at error message

--- src/schemas/login-schema.ts
+++ src/schemas/login-schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const loginSchema = z.object({
   email: z.email("Email inválido"),
-  password: z.string({error: "Contraseña requerida"}).min(6, "Minimo 6 caracteres"),
+  password: z.string({error: "Contraseña requerida"}).min(6, "Mínimo 6 caracteres"),
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
